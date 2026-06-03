bb4a8825267ed45fc1e07f84ff3f63440952c5c4
Chihaan <gavet.vadim@gmail.com>
2026-06-03 09:58:49 +0200

    add: new logic non logic

--- sql_crud/0-create_table.sql
+++ sql_crud/0-create_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE books (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    genre TEXT,
+    genre TEXT NOT NULL,
     stock INTEGER,
     price REAL,
     published_year INTEGER
